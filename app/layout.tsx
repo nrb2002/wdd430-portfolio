5f8423df78b98b2799ef1d8442778fadd7b50ff1
nrb2002 <tshibasubaron@yahoo.fr>
2026-07-07 20:29:46 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: "My Portfolio",
+  title: "My Portfolio - Baron Tshibasu",
   description: "Portfolio built with Next.js",
 };
 
